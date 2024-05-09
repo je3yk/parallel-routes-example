4fb80838498ead2ffd857809a7434e0c1a98f35e
Jędrzej Zawojski <jedrzej.zawojski@teacode.io>
2024-05-09 14:08:37 +0200

    Define baseUrl

--- src/lib/fetchClient.tsx
+++ src/lib/fetchClient.tsx
@@ -1,7 +1,19 @@
 import { ApiResponse } from "unsplash-js/dist/helpers/response"
 import { Photos } from "unsplash-js/dist/methods/search/types/response"
 
-const BASE_URL = 'http://localhost:3000/api'
+function getBaseUrl() {
+    if (typeof window !== 'undefined') {
+        return ''
+    }
+
+    if (process.env.VERCEL_URL) {
+        return `https://${process.env.VERCEL_URL}`
+    }
+
+    return `http://localhost:${process.env.PORT ?? 3000}`
+}
+
+const BASE_URL = `${getBaseUrl()}/api`
 
 type PhotosSearchResponse = { data: NonNullable<ApiResponse<Photos>['response']>['results'] | never[], total: number }
 
@@ -29,6 +41,6 @@ const getPhoto = async (id: string) => {
 }
 
 export {
-    searchPhotos,
-    getPhoto
+    getPhoto, searchPhotos
 }
+
