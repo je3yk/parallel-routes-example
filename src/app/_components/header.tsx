cd0c8906cc76ec4f2bc0cd1097acb695fbcda377
jedrzej.zawojski <jedrzej.zawojski95@gmail.com>
2024-05-12 12:04:50 +0200

    Delete gallery

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -14,9 +14,6 @@ export function Header() {
                 <SearchInput />
             </Suspense>
             <div className="flex items-center justify-end gap-2">
-                <Button variant="outline" asChild>
-                    <Link href="/gallery">Gallery</Link>
-                </Button>
                 <ModeToggle />
                 <Button variant="outline" asChild>
                     <Link href="https://github.com/je3yk/parallel-routes-example">GitHub</Link>
--- src/app/gallery/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { Skeleton } from "@/components/ui/skeleton";
-
-export default function GalleryLoading() {
-    return <Skeleton className="w-full h-full" />
-}
