5e6892f2a1c5c76b90653c30b9405136308ee853
jedrzej.zawojski <jedrzej.zawojski95@gmail.com>
2024-05-12 14:08:20 +0200

    add logger

--- src/app/(search)/layout.tsx
+++ src/app/(search)/layout.tsx
@@ -1,15 +1,14 @@
 import { LocalHistoryProvider } from "@/context/LocalHistory";
-import { Suspense } from "react";
 
 export default function SearchLayout({results, history}: {results: React.ReactNode, history: React.ReactNode}) {
     return  (
         <LocalHistoryProvider>
             <div className="flex w-full h-full min-h-0">
-                <div className="w-[70%] h-full flex-1 overflow-y-auto">
+                {/* <div className="w-[70%] h-full flex-1 overflow-y-auto">
                     <Suspense>
                         {results}
                     </Suspense>
-                </div>
+                </div> */}
                 <div className="w-[30%] h-full overflow-y-auto">
                     {history}
                 </div>
